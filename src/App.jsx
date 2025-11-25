c565e1c44ff1a99f281a4787bbe47b70d52c5710
hishamrehab <hishamrehab30@gmail.com>
2025-11-25 06:44:03 +0200

    installing required dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ gsap.registerPlugin(ScrollTrigger ,SplitText);
 const App = () => {
   return (
     <div>
-      App
-    </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>    </div>
   )
 }
 
